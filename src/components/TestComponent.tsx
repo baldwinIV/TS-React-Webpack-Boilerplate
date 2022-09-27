42579c631ab3ccfbd47716fc72cdadfc0e16bb0a
Kim Sanghun(BaldwinIV) <ksanghun10@gmail.com>
2022-09-27 23:11:49 +0900

    feat : React Component for Test

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './TestComponent.module.scss';
+export function TestComponent() {
+    return (
+        <div className={style.wrapper}>
+            image test
+            <div className={style.imageContainer}></div>
+        </div>
+    );
+}
